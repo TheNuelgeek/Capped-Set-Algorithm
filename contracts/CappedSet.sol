39d7b7ba38f07a5b7cd1b2b126dc0fc6d5c24199
TheNuelgeek <thenuelgeekmain@gmail.com>
2023-06-08 19:19:51 +0100

    The solidity task completed

--- contracts/CappedSet.sol
+++ contracts/CappedSet.sol
@@ -45,6 +45,8 @@ contract CappedSet {
             }else {
                 return (elementAddresses[lowestValueAddressIndex], lowestValue);
             }
+
+            
         }
 
         if (value == lowestValue) {
@@ -81,75 +83,79 @@ contract CappedSet {
     function update(address addr, uint256 newValue) external returns (address, uint256) {
         require(addr != address(0), "Invalid address");
         require(newValue > 0, "Value must be greater than 0");
+        require(addr == elementAddresses[elements[addr].index], "Address doesn't exist, use insert function");
+        require(newValue != elements[addr].value, "You can't update the value with the same value");
 
-        if (elements[addr].index == 0) {
-            elements[addr].value = newValue;
-            if (newValue < lowestValue) {
-                lowestValue = newValue;
-                return (addr, newValue);
-            } else {
-                return (address(0), 0);
-            }
+        elements[addr].value = newValue;
+
+        if (newValue < lowestValue) {
+            lowestValueAddressIndex = elements[addr].index;
+            lowestValue = newValue;
+            return (addr, newValue);
         }
 
-        if (newValue >= lowestValue) {
-            return (address(0), 0);
-        }
-
-        uint256 currentIndex = elements[addr].index;
-        address evictedAddress = elementAddresses[currentIndex];
-        elements[evictedAddress].value = newValue;
-        elements[evictedAddress].index = currentIndex;
-        elementAddresses[currentIndex] = addr;
-
-        uint256 currentLowest = elements[evictedAddress].value;
-        for (uint256 i = currentIndex + 1; i < elementAddresses.length; i++) {
-            address currentAddress = elementAddresses[i];
-            if (elements[currentAddress].value < currentLowest) {
-                currentLowest = elements[currentAddress].value;
-                evictedAddress = currentAddress;
-            }
-            elements[currentAddress].index = i - 1;
-        }
-
-        lowestValue = currentLowest;
-        return (evictedAddress, currentLowest);
+        return (elementAddresses[lowestValueAddressIndex], lowestValue);
     }
+
 
     function remove(address addr) external returns (address, uint256) {
         require(addr != address(0), "Invalid address");
         require(elementAddresses.length > 0, "Set is empty");
+        require(addr == elementAddresses[elements[addr].index], "Address doesn't exist");
 
-        if (elements[addr].index == 0) {
-            address evictedAddress = elementAddresses[0];
-            uint256 currentLowest = elements[evictedAddress].value;
-            for (uint256 i = 1; i < elementAddresses.length; i++) {
-                address currentAddress = elementAddresses[i];
-                if (elements[currentAddress].value < currentLowest) {
-                    currentLowest = elements[currentAddress].value;
-                    evictedAddress = currentAddress;
-                }
-                elements[currentAddress].index = i - 1;
-            }
+        uint256 indexToRemove = elements[addr].index;
+        uint256 valueOfIndexToRemove = elements[addr].value;
+        address lastAddress = elementAddresses[elementAddresses.length - 1];
 
-            delete elements[evictedAddress];
-            elementAddresses[0] = elementAddresses[elementAddresses.length - 1];
+        if (indexToRemove == elementAddresses.length - 1){
+            elementAddresses.pop();
+            delete elements[addr];
+        }else if( elementAddresses.length == 1){
+            elementAddresses.pop();
+            delete elements[addr];
+            lowestValueAddressIndex = 0;
+            lowestValue = 0;
+            return (address(0), 0);
+        }else{
+
+            elements[lastAddress].index = indexToRemove;
+            elementAddresses[indexToRemove] = lastAddress;
             elementAddresses.pop();
 
-            lowestValue = currentLowest;
-            return (evictedAddress, currentLowest);
+            delete elements[addr];
         }
 
-        address lastAddress = elementAddresses[elementAddresses.length - 1];
-        elements[lastAddress].index = elements[addr].index;
-        elementAddresses[elements[addr].index] = lastAddress;
-        elementAddresses.pop();
 
-        delete elements[addr];
-        return (address(0), 0);
+        if (valueOfIndexToRemove == lowestValue) {
+            (lowestValue, lowestValueAddressIndex) = findLowestValue();
+        } else if (elements[lastAddress].value == lowestValue) {
+            lowestValueAddressIndex = indexToRemove;
+        }
+
+        return (elementAddresses[lowestValueAddressIndex], lowestValue);
     }
+
+
 
     function getValue(address addr) external view returns (uint256) {
         return elements[addr].value;
     }
+
+    function findLowestValue() internal view returns (uint256, uint256) {
+        uint256 lowest = lowestValue;
+        uint256 lowestIndex = lowestValueAddressIndex;
+
+        for (uint256 i = 0; i < elementAddresses.length; i++) {
+            address currentAddress = elementAddresses[i];
+            uint256 currentValue = elements[currentAddress].value;
+            if (currentValue < lowest) {
+                lowest = currentValue;
+                lowestIndex = i;
+            }
+        }
+
+        return (lowest, lowestIndex);
+    }
+
+
 }
